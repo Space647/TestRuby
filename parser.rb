7929b8fa0ec356f287ac3b9ce48f09d832868152
Gleb <gleb.borisevich.97@gmail.com>
2017-09-08 18:23:38 +0300

    added download img

--- parser.rb
+++ parser.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'image_downloader'
 
 url = 'https://www.petsonic.com/snacks-huesos-para-perros/'
 html = open(url)
@@ -10,7 +11,15 @@ showing= node_found_by_xpath.xpath('@href')
 title = node_found_by_xpath.xpath('@title')
 ParsingUrl=open("#{showing}")
 doc1= Nokogiri::HTML(ParsingUrl)
+ing1=doc1.xpath('//*[@id="bigpic"]')
+img1=ing1.xpath('@src')
+pars1=doc1.xpath('//*[@id="attributes"]/fieldset/div/ul[2]/li/span[1]').text
+pars2=doc1.xpath('//*[@id="attributes"]/fieldset/div/ul[2]/li/span[2]').text
+File.open('1.png', 'wb') do |fo|
+    fo.write open("#{img1}").read 
+  end
 
-
-puts title
-puts showing
+puts img1
+#puts title
+#puts pars1
+#puts pars2
